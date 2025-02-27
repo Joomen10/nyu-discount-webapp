647fe7544fe56fba9ba37f33b5c541819d20aadd
Juno <jl12818@nyu.edu>
2025-02-27 18:56:58 -0500

    create structures of website

---
+++ restaurant/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import restaurant_view
+
+urlpatterns = [
+    path("", restaurant_view, name="map"),
+]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def restaurant_view(request):
+    return HttpResponse("Hello, this is the restaurant view!")
